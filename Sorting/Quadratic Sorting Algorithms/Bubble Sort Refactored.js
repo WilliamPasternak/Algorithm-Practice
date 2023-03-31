c961c9a3d9fdda63ba8c6fd7312b73356247bcd7
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2023-03-30 22:30:30 -0400

    Refactored Bubble Sort

--- Sorting/Quadratic Sorting Algorithms/Bubble Sort Refactored.js
+++ Sorting/Quadratic Sorting Algorithms/Bubble Sort Refactored.js
@@ -22,3 +22,27 @@ function bubbleSort(array) {
 function swap(i,j,array){
   [array[i],array[j]] = [array[j],array[i]]  
 }
+
+
+// Slight Optimization with counter variable so we don't need to check sorted numbers.
+
+// Time: O(n^2) Space: O(1)
+function bubbleSort(array) {
+   let isSorted = false
+   let counter = 0
+   while(!isSorted){
+     isSorted = true
+     for(let i = 0 ; i < array.length - 1 - counter; i++){
+      if(array[i] > array[i + 1]){
+        swap(i,i+1,array)
+        isSorted = false
+      }
+     }
+     counter++
+   }
+   return array
+}
+
+function swap(i,j,array){
+  [array[i],array[j]] = [array[j], array[i]]
+}
